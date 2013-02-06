4bc92ae71a687925e840dea7ee472e06be1b6af1
john.vanbreda <john.vanbreda@biodiverseit.co.uk>
2013-02-06 19:53:57 +0000

    Copy of Drupal 6 iform module, to act as a starting point for D7 version

---
+++ iform/iform.admin.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $('input[name="indicia_warehouse"]').change(function(evt) {
+    if (evt.target.value=='other') {
+      $('#warehouse_details').removeClass('collapsed');
+    } else {
+      $('#warehouse_details').addClass('collapsed');
+    }
+  });
+});
