35cc94864e390aadd5dd97d343fbfe78d682a77d
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2013-02-15 09:33:33 +0000

    First batch of changes to D7 module. Some of it works!

--- drupal_7/modules/iform/trunk/iform/iform.admin.js
+++ drupal_7/modules/iform/trunk/iform/iform.admin.js
@@ -1,9 +1,11 @@
-$(document).ready(function() {
-  $('input[name="indicia_warehouse"]').change(function(evt) {
-    if (evt.target.value=='other') {
-      $('#warehouse_details').removeClass('collapsed');
-    } else {
-      $('#warehouse_details').addClass('collapsed');
-    }
+(function ($) {(function ($) {
+  $(document).ready(function() {
+    $('input[name="indicia_warehouse"]').change(function(evt) {
+      if (evt.target.value=='other') {
+        $('#warehouse_details').removeClass('collapsed');
+      } else {
+        $('#warehouse_details').addClass('collapsed');
+      }
+    });
   });
-});
+})(jQuery);})(jQuery);
